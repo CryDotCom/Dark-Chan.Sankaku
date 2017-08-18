6ff09d17fcb2d5b7b90400c7243abf6aa214b9b7
CryDotCom <31138241+CryDotCom@users.noreply.github.com>
2017-08-18 18:59:20 +0200

    Update Dark-Script.js

--- Dark-Script.js
+++ Dark-Script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Dark chan.Sankaku
+// @name         Dark-chan.Sankaku
 // @namespace    http://tampermonkey.net/
 // @version      1.0.2
 // @description  try to take over the world!
